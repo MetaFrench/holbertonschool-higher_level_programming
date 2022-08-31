035f0cab0695f9c43f84a87126fa0cb1c70ab158
root <nick.french0307@gmail.com>
2022-08-30 19:32:13 -0700

    3print

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i is not 'q' and i is not 'e':
-        print("{:c}".format(i), end='')
+for a in range(ord('a'), ord('z') + 1):
+    print("{:c}".format(a), end='')
