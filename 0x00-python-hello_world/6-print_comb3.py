b9ada4617dd3fea8aad8dbffb8a7702378548213
root <nick.french0307@gmail.com>
2022-09-02 08:09:06 -0700

    print

--- 0x00-python-hello_world/6-print_comb3.py
+++ 0x00-python-hello_world/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-str1 = "Holberton"
-str2 = "School"
-str1 = str1 + " " + str2
-print("Welcome to {}!".format(str1))
+for i in range(10):
+    for j in range(10):
+        if i == 8 and j == 9:
+            print("89")
+        elif i < j:
+            print("{:d}{:d}".format(i, j), end=", ")
