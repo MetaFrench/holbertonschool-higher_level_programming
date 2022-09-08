2fe6d2607db747d8aa32d5dfe1c0d463f91ecdfd
root <nick.french0307@gmail.com>
2022-09-07 21:15:33 -0700

    3squarenew

--- 0x04-python-classes/3-square.py
+++ 0x04-python-classes/3-square.py
@@ -20,3 +20,12 @@ class Square:
     def size(self):
         """Getter for a square's size"""
         return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter for a square's size"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
