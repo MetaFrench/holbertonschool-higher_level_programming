f43204d4e5d43acc90adc18c0b8ffcc775e2134e
root <nick.french0307@gmail.com>
2022-09-07 21:33:35 -0700

    4square

--- 0x04-python-classes/4-square.py
+++ 0x04-python-classes/4-square.py
@@ -16,3 +16,17 @@ class Square:
     def area(self):
         """Prints area"""
         return self.__size ** 2
+
+    @property
+    def size(self):
+        """Getter for a square's size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter for a square's size"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size =
