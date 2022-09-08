0e5d2dd0744f229736318f8ddd78e2ec56fad5c7
root <nick.french0307@gmail.com>
2022-09-07 20:10:44 -0700

    2square

--- 0x04-python-classes/2-square.py
+++ 0x04-python-classes/2-square.py
@@ -11,3 +11,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        """Prints area"""
+        return self.__size ** 2
