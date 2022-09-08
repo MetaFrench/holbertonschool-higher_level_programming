70ddb0ef0f3b6ef8c2f29de3626325d9631e8b21
root <nick.french0307@gmail.com>
2022-09-07 20:06:27 -0700

    2square

---
+++ 0x04-python-classes/2-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""square class"""
+
+
+class Square:
+    """This class defines a square"""
+    def __init__(self, size=0):
+        """This initializes a square with a size"""
