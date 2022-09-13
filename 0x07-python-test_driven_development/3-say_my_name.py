bb33cb5a3f2074ad4d3b11f99800643a34bd4128
root <nick.french0307@gmail.com>
2022-09-12 17:44:39 -0700

    saymyname

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -4,3 +4,9 @@
 
 def say_my_name(first_name, last_name=""):
     """ Function that prints the input names """
+    if type(first_name) != str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) != str:
+        raise TypeError("last_name must be a string")
+
+    print("My name is", first_name, last_name)
