68d82209ef5432f77403ed615ae0d6abcdca16aa
root <nick.french0307@gmail.com>
2022-09-15 19:53:45 -0700

    my_list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module 1-my_list
+Writes a class 'MyList' that inherits from
+class 'list'
+"""
+
+
+class MyList(list):
+    """List with attributes of parent class"""
+
+    def print_sorted(self):
+        """Prints list in ascending order"""
+
+        print("{}".format((sorted)
