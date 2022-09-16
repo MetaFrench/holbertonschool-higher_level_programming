17bd66acfd99aa2bb8d59c89f327a0ac9079a8a0
root <nick.french0307@gmail.com>
2022-09-15 19:51:39 -0700

    lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module 0-lookup
+Function that returns the list of
+available attributes and methods
+"""
+
+
+def lookup(obj):
+    """"Returns list of attributes and methods"""
+
+   
