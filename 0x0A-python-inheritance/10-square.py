fa440952087f9b6ce1951d3259932b9b2e80179d
root <nick.french0307@gmail.com>
2022-09-15 20:53:48 -0700

    square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" 7-base_geometry Module """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Square class that extends Rectangle """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """ returns the area of the Square"""
+        return self.__size ** 2
