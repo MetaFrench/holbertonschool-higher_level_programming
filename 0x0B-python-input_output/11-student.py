54f935f80055ba2e40fa40ae97a8736f1f4257d6
root <nick.french0307@gmail.com>
2022-09-19 20:09:15 -0700

    student

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -18,3 +18,16 @@ class Student:
         dict = vars(self)
         if attrs is None:
             return dict
+
+        studentInfo = {}
+        for item in attrs:
+            if item in dict:
+                studentInfo[item] = dict[item]
+        return studentInfo
+
+    def reload_from_json(self, json):
+        """
+        Function that replaces all attributes of the
+        Student instance.
+        """
+        self.__dict__.update(json)
