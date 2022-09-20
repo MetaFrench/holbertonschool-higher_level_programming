eb54a9dba73f616255aa47c06c887e66c0762627
root <nick.french0307@gmail.com>
2022-09-19 19:21:26 -0700

    add

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,13 +8,13 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 fname = "add_item.json"
-ListFile = []
+ListyBoii = []
 
 try:
-    ListFile = load_from_json_file(fname)
+    ListyBoii = load_from_json_file(fname)
 except:
     pass
 
 for i in range(1, len(sys.argv)):
-    ListFile.append(sys.argv[i])
-save_to_json_file(ListFile, fname)
+    ListyBoii.append(sys.argv[i])
+save_to_json_file(ListyBoii, fname)
