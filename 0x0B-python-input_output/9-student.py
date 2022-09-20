71e06ea4b625008e82e99a0044ee2ea4c0859105
root <nick.french0307@gmail.com>
2022-09-19 19:42:44 -0700

    student

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -10,3 +10,9 @@ class Student:
         self.last_name = last_name
         self.age = age
 
+    def to_json(self):
+        """
+        Retrieves a dictionary representation of
+        a Student instance
+        """
+        return vars(self)
