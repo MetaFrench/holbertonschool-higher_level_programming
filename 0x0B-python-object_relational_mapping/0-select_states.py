bcba37e67b346945db45f22b2395932fbe213321
root <nick.french0307@gmail.com>
2022-10-19 18:34:37 -0700

    zero

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
-"""uses mysqldb to print all states from database"""
-import MySQLdb
-import sys
+""" Lists all states from the database hbtn_0e_0_usa """
 
+if __name__ == "__main__":
 
-def main(argv):
-    """main function to guard content"""
-    if len(argv) != 4:
-        return ()
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+
     cur = db.cursor()
-    cur.execute("SELECT * FROM states")
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
     rows = cur.fetchall()
     for row in rows:
         print(row)
     cur.close()
     db.close()
-
-
-if __name__ == '__main__':
-    main(sys.argv)
