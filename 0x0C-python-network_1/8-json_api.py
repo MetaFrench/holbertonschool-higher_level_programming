b73e463494b5d428a3bae09dbaecfe87a9aa553d
root <nick.french0307@gmail.com>
2022-11-01 21:10:10 -0700

    json

--- 0x0C-python-network_1/8-json_api.py
+++ 0x0C-python-network_1/8-json_api.py
@@ -1,5 +1,26 @@
 #!/usr/bin/python3
-""" Script that takes in a letter and sends a POST request
-to http://0.0.0.0:5000/search_user with the letter as a parameter """
-import requests
-import sys
+"""sends a post response and checks for json validity"""
+
+
+def main(argv):
+    import requests
+
+    data = {}
+    if len(argv) > 1:
+        data['q'] = argv[1]
+    else:
+        data['q'] = ""
+
+    r = requests.post('http://0.0.0.0:5000/search_user', data=data)
+    try:
+        j = r.json()
+        if j.get('id') is None:
+            print("No result")
+        else:
+            print("[{}] {}".format(j.get('id'), j.get('name')))
+    except Exception:
+            print("Not a valid JSON")
+
+if __name__ == "__main__":
+    from sys import argv
+    main(argv)
