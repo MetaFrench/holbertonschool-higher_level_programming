cca9173a1ffbc4641f56862ac31528bf8d30b53f
root <nick.french0307@gmail.com>
2022-11-01 18:20:12 -0700

    post

--- 0x0C-python-network_1/2-post_email.py
+++ 0x0C-python-network_1/2-post_email.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
-""" Takes in a URL and an email, sends a POST request to the passed URL
-with the email as a parameter, and displays the body of the response
+""" Takes in URL and email, sends POST request to passed URL
+with email as parameter, displays body of response
 (decoded in utf-8) """
 import sys
 from urllib import request, parse
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {'email': sys.argv[2]}
+    data = parse.urlencode(value).encode('utf-8')
+    req = request.Request(url, data)
+    with request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
