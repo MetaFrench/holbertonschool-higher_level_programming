cddbb7c9b15c06c1c80e96260054303a8319fc5e
root <nick.french0307@gmail.com>
2022-11-01 17:58:49 -0700

    header

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -4,3 +4,9 @@
  found in the header of the response """
 from urllib import request
 import sys
+
+
+if __name__ == "__main__":
+    req = request.Request(sys.argv[1])
+    with request.urlopen(req) as response:
+        print(response.info()['X-Request-Id'])
