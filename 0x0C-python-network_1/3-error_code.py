e283e9f389272757dc6d3aa93165d322eb2c5339
root <nick.french0307@gmail.com>
2022-11-01 18:24:43 -0700

    error

--- 0x0C-python-network_1/3-error_code.py
+++ 0x0C-python-network_1/3-error_code.py
@@ -2,3 +2,13 @@
 """ Manages urllib.error.HTTPError exceptions """
 from urllib import request, error
 import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = request.Request(url)
+    try:
+        with request.urlopen(req) as response:
+            print(response.read().decode('utf-8'))
+    except error.HTTPError as e:
+        print('Error code: {}'.format(e.code))
